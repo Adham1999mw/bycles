05c9d40348f08b61f501c1597a19c1d8f0e3b7da
Adham Elmallwany <adham.elmallwany@Homzmart.local>
2024-04-30 03:27:09 +0300

    [feature/HOM-9074] sla

--- pages/contact/hook/contactHook.js
+++ pages/contact/hook/contactHook.js
@@ -12,41 +12,41 @@ const useContactHook = () => {
   const [loading, setLoading] = useState(false);
 
   const onSubmit = async () => {
-    if (data &&  Cookies.get('hour')) {
-      setLoading(true)
-      const response = await fetch(
-        "https://formsubmit.co/ajax/adhamelmalawany@gmail.com",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: data.name,
-            email: data.email,
-            phone:data.phone,
-            Object : data?.objectInput,
-            description: data?.desc,
-            day: Cookies.get('day'),
-            month:Cookies.get('month'),
-            hour:Cookies.get('hour')
-            // Add other form fields as needed
-          }),
-        }
-      );
+    // if (data &&  Cookies.get('hour')) {
+    //   setLoading(true)
+    //   const response = await fetch(
+    //     "https://formsubmit.co/ajax/adhamelmalawany@gmail.com",
+    //     {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({
+    //         name: data.name,
+    //         email: data.email,
+    //         phone:data.phone,
+    //         Object : data?.objectInput,
+    //         description: data?.desc,
+    //         day: Cookies.get('day'),
+    //         month:Cookies.get('month'),
+    //         hour:Cookies.get('hour')
+    //         // Add other form fields as needed
+    //       }),
+    //     }
+    //   );
 
-      if (response.ok) {
-        setLoading(false)
-        // enqueueSnackbar("Form submitted successfully", { variant: "success" });
-        Cookies.remove('day'),
-        Cookies.remove('month'),
-        Cookies.remove('hour')
-      } else {
-        // enqueueSnackbar("Failed to submit form", { variant: "error" });
-      }
-    } else {
-      // return enqueueSnackbar("please select date");
-    }
+    //   if (response.ok) {
+    //     setLoading(false)
+    //     // enqueueSnackbar("Form submitted successfully", { variant: "success" });
+    //     Cookies.remove('day'),
+    //     Cookies.remove('month'),
+    //     Cookies.remove('hour')
+    //   } else {
+    //     // enqueueSnackbar("Failed to submit form", { variant: "error" });
+    //   }
+    // } else {
+    //   // return enqueueSnackbar("please select date");
+    // }
   };
 
   const onSubmitError = () => {
@@ -54,12 +54,12 @@ const useContactHook = () => {
   };
 
   const handleInputChange = (field = "", obj, value) => {
-    setData((prevUserAuth) => ({ ...prevUserAuth, ...obj }));
-    field.onChange(value);
+    // setData((prevUserAuth) => ({ ...prevUserAuth, ...obj }));
+    // field.onChange(value);
   };
 
   const setDataFun = (dateTime) => {
-    setData((prevUserAuth) => ({ ...prevUserAuth, ...dateTime }));
+    // setData((prevUserAuth) => ({ ...prevUserAuth, ...dateTime }));
   };
 
   return {
